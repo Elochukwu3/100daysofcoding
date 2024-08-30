2ad0fa0e8ce63ae095bb2d3e5dc9eaffd1820233
Elochukwu3 <oguajuromauld5@gmail.com>
2024-08-30 10:49:43 +0100

    clean up

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.listen(3000, () => {
-  console.log(`Server is running on port 3000 http://localhos:3000`);
+  console.log('Server is running on port 3000, local');
 });
