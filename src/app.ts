ddb17ca69803f3a434e2531297432af75c25f423
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-05 23:00:46 +0100

    fx-#107: fix type erros on app

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 import 'dotenv/config';
 import 'express-async-errors';
 import express, { Request, Response } from 'express';
-import authRoute from './modules/auth/routes/auth.routes';
+import authRoute from '@auth/routes/auth.routes';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-// import corsOptions from './config/corsOptions';
+import corsOptions from '@common/config/corsOptions.config'
 // import connectDB from './config/dbConn';
 import mongoose from 'mongoose';
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors(corsOptions));
+
 
 
 app.get('/', (req: Request, res: Response) => {
