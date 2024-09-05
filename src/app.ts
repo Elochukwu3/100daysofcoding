404876de332e04c8132c29c23e2876e117cd4916
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-05 21:35:33 +0100

    ft-#107: update server setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
+import 'dotenv/config';
+import 'express-async-errors';
 import express, { Request, Response } from 'express';
-import authRoute from './modules/auth/routes/auth.routes'
+import authRoute from './modules/auth/routes/auth.routes';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+// import corsOptions from './config/corsOptions';
+// import connectDB from './config/dbConn';
+import mongoose from 'mongoose';
 
 const app = express();
+app.use(cookieParser());
+app.use(express.json());
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express!');
