e5771d0ce7de98c5ab9a7ea25e3402c893fad473
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-07 08:49:24 +0100

    ft-#107: Add db setup

---
+++ src/modules/auth/controllers/verifyOtp.ts
@@ -0,0 +1,11 @@
+export const verifyOtp = async (req: Request, res: Response): Promise<void> => {
+    try {
+        // const { email, otp } = req.body;
+    
+    
+
+     
+    } catch (error) {
+       
+    }
+};
--- src/modules/common/config/db.config.ts
+++ src/modules/common/config/db.config.ts
@@ -1,1 +1,27 @@
-const DB_URL = 'http://localhost';
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connect(process.env.DB_URL as string);
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
+        // logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log');
+        process.exit(1); 
+    }
+};
+
+connectDB();
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to DB');
+});
+
+mongoose.connection.on('error', err => {
+    console.error(`Mongoose connection error: ${err}`);
+    // logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log');
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});
