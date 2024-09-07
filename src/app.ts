7a5970130dce1dffd8e50ac70131fdb4f0227e0d
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-07 08:48:14 +0100

    ft-#107: update app settings

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import 'express-async-errors';
 import express, { Request, Response } from 'express';
-import authRoute from '@auth/routes/auth.routes';
+import authRoute from './modules/auth/routes/auth.routes'
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import corsOptions from '@common/config/corsOptions.config'
+import corsOptions from './modules/common/config/corsOptions.config';''
+import morgan from 'morgan';
+import helmet from 'helmet';
 // import connectDB from './config/dbConn';
 import mongoose from 'mongoose';
 
@@ -13,13 +15,37 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(cors(corsOptions));
 
+app.use(helmet());
+(() => {
+  if (process.env.NODE_ENV === "test") return; 
+
+  const morganMiddleware = (() => {
+    if (process.env.NODE_ENV === "development") {
+      return morgan("dev");
+    }
+
+    if (process.env.NODE_ENV === "production") {
+      return morgan("combined");
+    }
+
+    return null; 
+  })();
+
+  if (morganMiddleware) {
+    app.use(morganMiddleware); 
+  }
+})();
+
+
+
 
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express!');
 });
-app.use('/api', authRoute);
+app.use('/auth/v1', authRoute)
+// app.use('/auth/v1', authRoute);
 app.listen(3000, () => {
-  console.log('Server is running on port 3000, local');
+  console.log('Server is running on port 3000');
 });
   
