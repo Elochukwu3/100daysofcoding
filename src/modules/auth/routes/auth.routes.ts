6d1a15264e1ccd9fc6706af581343150727ed39d
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-07 21:58:15 +0100

    ft-#108: update auth routes

--- src/modules/auth/routes/auth.routes.ts
+++ src/modules/auth/routes/auth.routes.ts
@@ -1,10 +1,12 @@
 
 import { Router, Request, Response } from 'express';
 import getOtp from '../controllers/getOtp';
+import verifyOtp from '../controllers/verifyOtp';
 
 const router = Router();
 
 
 router.get('/get-otp', getOtp)
+router.post('/verify-otp', verifyOtp)
 
 export default router;
