0fa6c30b7a362ee0d862ce05f1ae9030316ae20c
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-07 21:51:06 +0100

    ft-#107: update get otp controller

--- src/modules/auth/controllers/getOtp.ts
+++ src/modules/auth/controllers/getOtp.ts
@@ -1,37 +1,52 @@
 // src/modules/auth/controllers/auth.controller.ts
 
-import { Request, Response } from 'express';
-import {Token, validateEmail} from '../models/token'
-import sendOTPEmail from '../../common/utils/sendEmail';
-import { generateOtp } from '../../common/utils/generateToken';
-// import { generateOtp, verifyOtp } from '../services/auth.service'; // Implement these services as needed
+import { Request, Response } from "express";
+import { Token, validateEmail } from "../models/token";
+import sendOTPEmail from "../../common/utils/sendEmail";
+import { generateOtp } from "../../common/utils/generateToken";
 
-export const getOtp = async (req: Request, res: Response): Promise<Response | undefined> => {
-    try {
-        const { email } = req.query;
-        const{ error} = validateEmail({ email: email as string });
-        if (error) {
-            return res.status(400).json({
-                "status": "Bad request",
-                "message": error?.message.replace(/\"/g, ''),
-                'statusCode': "400"
-            });
-        }
-        const OTP = await generateOtp() 
-       const response = await sendOTPEmail(email as string, OTP)
-        
-        return res.status(200).json({ 
-                "status": "success",
-                "message": "OTP sent successfully",
-                "data": {
-                    "otp": '83990'
-                },
-                "statusCode": "200"
-         });
-    } catch (error) {
-        res.status(500).json({  "status": "Bad request", message: 'Internal server error', error, statusCode: "500" });
+export const getOtp = async (
+  req: Request,
+  res: Response
+): Promise<Response | undefined> => {
+  try {
+    const { email } = req.query;
+    const { error } = validateEmail({ email: email as string });
+    if (error) {
+      return res.status(400).json({
+        status: "Bad request",
+        message: error?.message.replace(/\"/g, ""),
+        statusCode: "400",
+      });
     }
+    const OTP = await generateOtp();
+
+    await Token.create({
+      email: email,
+      token: OTP,
+      purpose: "email_verification",
+    });
+
+    await sendOTPEmail(email as string, OTP);
+
+    return res.status(200).json({
+      status: "success",
+      message: "OTP sent successfully",
+      data: {
+        otp: OTP,
+      },
+      statusCode: "200",
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({
+        status: "Bad request",
+        message: "Internal server error",
+        error,
+        statusCode: "500",
+      });
+  }
 };
 
-export default getOtp
-
+export default getOtp;
