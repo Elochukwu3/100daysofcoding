323b7e8450f7fb3199be7d76d4469d0096f139d5
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-07 08:48:55 +0100

    ft-#107: Add otp helper functions

--- src/modules/auth/controllers/getOtp.ts
+++ src/modules/auth/controllers/getOtp.ts
@@ -1,24 +1,37 @@
 // src/modules/auth/controllers/auth.controller.ts
 
 import { Request, Response } from 'express';
+import {Token, validateEmail} from '../models/token'
+import sendOTPEmail from '../../common/utils/sendEmail';
+import { generateOtp } from '../../common/utils/generateToken';
 // import { generateOtp, verifyOtp } from '../services/auth.service'; // Implement these services as needed
 
-export const getOtp = async (req: Request, res: Response): Promise<void> => {
+export const getOtp = async (req: Request, res: Response): Promise<Response | undefined> => {
     try {
-        const { emailOrPhone } = req.query;
-
-        if (!emailOrPhone || typeof emailOrPhone !== 'string') {
-            res.status(400).json({ message: 'Invalid request' });
-            return;
+        const { email } = req.query;
+        const{ error} = validateEmail({ email: email as string });
+        if (error) {
+            return res.status(400).json({
+                "status": "Bad request",
+                "message": error?.message.replace(/\"/g, ''),
+                'statusCode': "400"
+            });
         }
-
-        // Generate and send OTP
-        // const otp = await generateOtp(emailOrPhone);
-        // Send OTP via email or SMS here (implementation depends on your setup)
-
-        res.status(200).json({ message: 'OTP sent successfully' });
+        const OTP = await generateOtp() 
+       const response = await sendOTPEmail(email as string, OTP)
+        
+        return res.status(200).json({ 
+                "status": "success",
+                "message": "OTP sent successfully",
+                "data": {
+                    "otp": '83990'
+                },
+                "statusCode": "200"
+         });
     } catch (error) {
-        res.status(500).json({ message: 'Internal server error', error });
+        res.status(500).json({  "status": "Bad request", message: 'Internal server error', error, statusCode: "500" });
     }
 };
+
+export default getOtp
 
