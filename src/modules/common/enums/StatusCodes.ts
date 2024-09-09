87b59e97e1a9786bf94bfd69526b0b33140245b2
slimzyBoom <slimzyboom@gmail.com>
2024-09-08 21:13:42 -1200

    added the enums

---
+++ src/modules/common/enums/StatusCodes.ts
@@ -0,0 +1,11 @@
+enum HttpStatus {
+  Success = 200,
+  Created = 201,
+  NoContent = 204,
+  BadRequest = 400,
+  Conflict = 409,
+  Unauthorized = 401,
+  NotFound = 404,
+  Forbiddden = 403,
+  ServerError = 500,
+}
