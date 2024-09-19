ee1e319aa644fa75ae9e796df1cb5d28bb427067
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-19 21:09:25 +0100

    fx-#108: fix database error handling format

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import "dotenv/config";
 import "express-async-errors";
-import "./modules/common/config/db.config";
 import express, { Request, Response } from "express";
 import authRoute from "./modules/auth/routes/auth.routes";
 import cookieParser from "cookie-parser";
@@ -9,9 +8,8 @@ import corsOptions from "./modules/common/config/corsOptions.config";
 import morgan from "morgan";
 import helmet from "helmet";
 import sessionConfig from "./modules/common/config/sessionConfig";
-// import connectDB from "./modules/common/config/db.config";
-import mongoose from "mongoose";
-
+import  "./modules/common/config/db.config";
+import errorHandler from "./modules/common/middlewares/errorHandler";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -24,8 +22,9 @@ app.use(sessionConfig);
 
 app.use(helmet());
 (() => {
+  
   if (process.env.NODE_ENV === "test") return;
-
+  
   const morganMiddleware = (() => {
     if (process.env.NODE_ENV === "development") {
       return morgan("dev");
@@ -49,7 +48,7 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/auth/v1", authRoute);
 
-
+app.use(errorHandler)
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
