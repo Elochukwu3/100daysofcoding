2b3e6dc8cb776eaf7905d47dee2dd35c90333183
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-23 07:55:08 +0100

    #102-reqpass: update sendEmail function

--- src/modules/common/utils/sendEmail.ts
+++ src/modules/common/utils/sendEmail.ts
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
 
-const sendOTPEmail = async (email: string, token: string) => {
+const sendOTPEmail = async (email: string, token: string, message:string) => {
   try {
     let transporter = nodemailer.createTransport({
       service: "gmail",
@@ -16,14 +16,14 @@ const sendOTPEmail = async (email: string, token: string) => {
     
             <h1 style="color: #333;">Ecommerce</h1>
     
-            <p>Your one-time Email verification code is:</p>
+            <p>{message}</p>
     
             <h2 style="color: #333; font-size: 24px;">${token}</h2>
     
             <p>Copy the link and Paste it to verify the Email</p>
     
             <div style="margin-top: 20px;">
-                <p style="font-size: 14px; color: #666;">Developed by Kariebi</p>
+                <p style="font-size: 14px; color: #666;">ecommerce.fav.jb.com</p>
             </div>
         </body>
     </html>    
