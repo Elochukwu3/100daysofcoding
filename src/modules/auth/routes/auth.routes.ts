f671994c0fa6090553423cc5fc26331320972891
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-23 08:28:34 +0100

    #103-password: update auth routes

--- src/modules/auth/routes/auth.routes.ts
+++ src/modules/auth/routes/auth.routes.ts
@@ -5,6 +5,9 @@ import verifyOtp from "../controllers/verifyOtp";
 import {refreshToken} from "../controllers/refreshToken";
 import changePassword from "../controllers/changePassword";
 import verifyUserAcces from "../../common/middlewares/verifyaccess";
+import verifyResetOtp from "../../auth/controllers/verifyResetOtp";
+import resetPassword from "../../auth/controllers/resetPswd";
+import requestPasswordReset from "../../auth/controllers/reqResetPswd";
 
 
 const router = Router();
@@ -14,6 +17,11 @@ router.post("/register", registerUser);
 router.post("/verify-otp", verifyOtp);
 router.post("/login", loginUser);
 router.post("/refresh-token", refreshToken);
-router.post("/change-password", verifyUserAcces ,changePassword);
+router.use(verifyUserAcces)
+router.post("/change-password", changePassword);
+router.post("/request-password-reset", requestPasswordReset);
+router.post("/verify-reset-otp", verifyResetOtp);
+router.post("/reset-password",  resetPassword);
+
 
 export default router;
