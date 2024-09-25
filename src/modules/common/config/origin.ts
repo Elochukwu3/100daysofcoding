bd505767bc42147aa77ca47526bd769db2a5f7b9
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-25 01:03:06 +0100

    fix: cors error on deployment

--- src/modules/common/config/origin.ts
+++ src/modules/common/config/origin.ts
@@ -1,5 +1,8 @@
 const origins: string[] = [
     'http://localhost:5173',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://100daysofcoding-production.up.railway.app'
 ];
+
 export default origins;
