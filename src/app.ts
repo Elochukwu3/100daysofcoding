21a556508f4ae0f9158a67e099b35405d1f45364
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-25 22:59:20 +0100

    clea up

--- src/app.ts
+++ src/app.ts
@@ -58,5 +58,5 @@ app.use("/auth/v1",apiKeyMiddleware, authRoute);
 
 app.use(errorHandler)
 app.listen(PORT, () => {
-  console.log('Server is running on port 3000');
+  console.log(`Server is running on port ${PORT}`);
 });
