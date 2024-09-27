91206e77419a6ce8bca397171368d1078d761bed
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-27 21:26:11 +0100

    ft-#1001: update state controller

--- src/modules/states/controller/state.controller.ts
+++ src/modules/states/controller/state.controller.ts
@@ -1,10 +1,12 @@
+
 import { Request, Response } from 'express';
-import { getAllStates, getStateById } from '../services/state.service';
+import { getAllStates, getStateById, getStateByName } from '../services/state.service';
+import { HttpStatus } from '../../common/enums/StatusCodes';
 
 export const getStates = (req: Request, res: Response) => {
   const states = getAllStates();
   if (!states.length) {
-    return res.status(404).json({ message: 'No states found' });
+    return res.status(HttpStatus.BadRequest).json({ message: 'No states found' });
   }
   
   res.json(states);
@@ -14,7 +16,28 @@ export const getState = (req: Request, res: Response) => {
   const stateId = parseInt(req.params.id, 10);
   const state = getStateById(stateId);
   if (!state) {
-    return res.status(404).json({ message: 'State not found' });
+    return res.status(HttpStatus.BadRequest).json({ message: 'State not found' });
   }
-  res.json(state);
+  res.json({
+    "status": "success",
+    "data": state,
+    "message": "State found successfully"
+  });
+};
+
+export const getByName =  (req: Request, res: Response) => {
+  const stateName = req.params.name;
+
+
+  try {
+    const state = getStateByName(stateName);
+    res.json({
+      "status": "success",
+      "data": state,
+      "message": "State found successfully"
+    });
+  } catch (error) {
+    const errorMessage = (error as Error).message;
+    res.status(404).json({ message: errorMessage });
+  }
 };
