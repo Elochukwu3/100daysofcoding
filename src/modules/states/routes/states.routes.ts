10eb3a78321bd4497e4973dc750abf46b606b605
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-27 21:26:45 +0100

    fx-#1001: fix state route [error]

--- src/modules/states/routes/states.routes.ts
+++ src/modules/states/routes/states.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { getStates, getState } from '../controller/state.controller';
+import { getStates, getState, getByName } from '../controller/state.controller';
 
 const router = Router();
 
+// Get all states
 router.get('/states', getStates);
-router.get('/states/:id', getState);
+router.get('/states/:id(\\d+)', getState);
+router.get('/states/:name([a-zA-Z]+)', getByName);
 
 export default router;
