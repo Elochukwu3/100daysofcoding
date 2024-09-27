17b00409b933c6c3e78717b97bad19f9f72e9be3
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-27 21:27:04 +0100

    ft-#1001: update state service funtion

--- src/modules/states/services/state.service.ts
+++ src/modules/states/services/state.service.ts
@@ -1,13 +1,13 @@
 import * as fs from 'fs';
 import * as path from 'path';
-
+import { NigeriaState } from 'modules/interfaces/State';
 
 const nigeriaData = JSON.parse(
   fs.readFileSync(path.join(__dirname, '../utils/nigeria.json'), 'utf-8')
 );
 
 
-export const getAllStates = () => {
+export const getAllStates = ():NigeriaState[] => {
   return nigeriaData.map((state: any, index: number) => ({
     id: index + 1, 
     state: state.state,
@@ -29,6 +29,23 @@ export const getStateById = (id: number) => {
     alias: state.alias,
     lgas: state.lgas.map((lga: string, lgaIndex: number) => ({
       id: lgaIndex + 1,
+      name: lga
+    }))
+  };
+};
+export const getStateByName = (stateName: string): any => {
+  const state = nigeriaData.find((s: NigeriaState) => s.state.toLowerCase() === stateName.toLowerCase());
+  
+  if (!state) {
+    throw new Error(`State ${stateName} not found`);
+  }
+
+  return {
+    id: nigeriaData.findIndex((s: NigeriaState) => s.state.toLowerCase() === stateName.toLowerCase()) + 1,
+    state: state.state,
+    alias: state.alias,
+    lgas: state.lgas.map((lga: string, lgaIndex: number) => ({
+      id: lgaIndex + 1, 
       name: lga
     }))
   };
