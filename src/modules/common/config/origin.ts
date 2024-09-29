81973825fe5d79477d46f67fc4799c12286f6791
Elochukwu3 <oguajuromauld5@gmail.com>
2024-09-29 23:09:42 +0100

    clean fix

--- src/modules/common/config/origin.ts
+++ src/modules/common/config/origin.ts
@@ -2,6 +2,7 @@ const origins: string[] = [
     'http://localhost:5173',
     'http://localhost:3000',
     'http://localhost:3001',
+    'http://127.0.0.1:3000/',
     'https://100daysofcoding-production.up.railway.app'
 ];
 
