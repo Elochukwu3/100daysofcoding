294a30364ef70d2d746bd68a69efa2377641d6c4
Elochukwu3 <oguajuromauld5@gmail.com>
2024-10-05 04:32:50 +0100

    ft-#109: update user routes

---
+++ src/modules/user/routes/user.route.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { getUserProfile } from '../controllers/UserProfile'; // Import your controller functions
+import verifyUserAcces from "../../common/middlewares/verifyaccess";
+// import { updateUserProfile } from '../controllers/userController'; // Update the import paths as needed
+// import { getUserOrders } from '../controllers/orderController'; // Import for orders
+
+const router = Router();
+
+router.use(verifyUserAcces);
+router.get('/profile/:userId', getUserProfile);
+
+// router.put('/api/v1/profile/:userId', verifyUser, updateUserProfile);
+
+// router.get('/api/v1/orders/:userId', verifyUser, getUserOrders);
+// router.put('/api/v1/payment/:userId', verifyUser, updatePaymentInformation);
+
+
+export default router;
