c03b280b9980bd67401b2d066d7ee9d8c809b60f
Elochukwu3 <oguajuromauld5@gmail.com>
2024-10-08 18:36:11 +0100

    clean up

--- src/modules/common/utils/validatePassword.ts
+++ src/modules/common/utils/validatePassword.ts
@@ -4,8 +4,7 @@ export const validatePassword = async (
   password: string,
   hash: string
 ): Promise<boolean> => {
-  console.log("Password:", password);
-  console.log("Hash:", hash);
+ 
 
   if (!password || !hash) {
     throw new Error("Both password and hash must be provided");
