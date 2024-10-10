b9dcdaa1dd44fa9a1da3aab858dd2ddc42167cfd
Elochukwu3 <oguajuromauld5@gmail.com>
2024-10-10 11:26:24 +0100

    ft-#115: update  new otp routes

--- src/modules/auth/routes/auth.routes.ts
+++ src/modules/auth/routes/auth.routes.ts
@@ -14,17 +14,13 @@ import newRegistrationOtp from "../../auth/controllers/sendNewregOtp";
 
 
 const router = Router();
-
-// import refreshToken from "../controllers/refreshToken";
-// import changePassword from "../controllers/changePassword";
-
 router.use(otpSessionConfig);
 
 router.post("/register", registerUser);
 router.post("/verify-otp", verifyOtp);
 router.post("/login", loginUser);
 router.post("/refresh-token", refreshToken);
-router.post('/auth/v1/generate-otp', otpRateLimiter, newRegistrationOtp);
+router.post('/generate-otp', otpRateLimiter, newRegistrationOtp);
 
 router.use(verifyUserAcces)
 router.post("/change-password", changePassword);
