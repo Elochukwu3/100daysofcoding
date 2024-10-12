5d6911bd230dab313a52e8a5370e7a0e75029191
Elochukwu3 <oguajuromauld5@gmail.com>
2024-10-12 13:28:07 +0100

    resolve file conflict

--- src/modules/common/config/origin.ts
+++ src/modules/common/config/origin.ts
@@ -3,7 +3,7 @@ const origins: string[] = [
     'http://localhost:3000',
     'http://localhost:3001',
     'http://127.0.0.1:3000', 
-    'https://100daysofcoding-production.up.railway.app',//production endpoint
-    'https://one00daysofcoding.onrender.com'
+    'https://one00daysofcoding.onrender.com',
+    'https://100daysofcoding-production.up.railway.app'
   ];
   export default origins;
