1c988f91901698289aec21ef071278808338c893
Elochukwu3 <oguajuromauld5@gmail.com>
2024-10-12 12:53:48 +0100

    fixing session bug

--- src/modules/common/config/origin.ts
+++ src/modules/common/config/origin.ts
@@ -2,7 +2,8 @@ const origins: string[] = [
     'http://localhost:5173',
     'http://localhost:3000',
     'http://localhost:3001',
-    'http://127.0.0.1:3000',  // Removed trailing slash
-    'https://100daysofcoding-production.up.railway.app'
+    'http://127.0.0.1:3000', 
+    'https://100daysofcoding-production.up.railway.app',//production endpoint
+    'https://one00daysofcoding.onrender.com'
   ];
   export default origins;
