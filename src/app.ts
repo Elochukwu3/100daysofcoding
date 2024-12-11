21d0c99cb5db70894214af5878a4d086df0e8c52
Elochukwu3 <oguajuromauld5@gmail.com>
2024-12-11 13:21:23 +0100

    ft-#120: cleaan up cart app

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ import googleAuth from "./modules/auth/routes/google.routes";
 import cookieParser from "cookie-parser";
 import googleAuthSessionConfig from "./modules/common/config/googleSessionConfig";
 import otpSessionConfig from "./modules/common/config/otpSessionConfig";
-import cartRoute from "./modules/products/routes/cart.route";
+import cartRoute from "./modules/cart/routes/cart.route";
 import deliveryRoute from "./modules/delivery-add/deliveryAdd.route"
 
 import morgan from "morgan";
